58aa72c87b8525cfd24a89801f039d985786e302
glenneduardordiz22-bit <glenneduardordiz22@gmail.com>
2026-02-11 19:33:27 +0800

    FEAT:Layout done

--- Ordiz_github_calculator_sample/Form1.cs
+++ Ordiz_github_calculator_sample/Form1.cs
@@ -16,5 +16,20 @@ namespace Ordiz_github_calculator_sample
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
